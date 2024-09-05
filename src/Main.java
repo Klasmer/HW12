c0a0083793ad4de6d4e1fbb781e68d5b231ce06f
klasmer <klasmer495@gmail.com>
2024-09-05 19:32:54 +0300

    Обьекты и классы. Задача 1

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main {
         Author Popova = new Author("Света", "Попова");
         Book book1 = new Book("Жизнь", Ivanov, 2015);
         System.out.println("Название книги = " + book1.getNameBook());
-        System.out.println("автор книги = " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+        System.out.println("Автор книги = " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
         System.out.println("Год выпуска книги = " + book1.getYearOfPublication());
         book1.setYearOfPublication(2024);
         System.out.println("Год перевыпуска книги = " + book1.getYearOfPublication());
         Book book2 = new Book("Огонь", Popova, 2017);
         System.out.println("Название книги = " + book2.getNameBook());
-        System.out.println("автор книги = " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
+        System.out.println("Автор книги = " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
         System.out.println("Год выпуска книги = " + book2.getYearOfPublication());
         book2.setYearOfPublication(2020);
+        System.out.println("Год перевыпуска книги = " + book2.getYearOfPublication());
     }
 }
